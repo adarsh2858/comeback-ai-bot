a2c338ab6d419dbb29177d2a6321e29ca31a19fe
adarsh2858 <adarsh2858@gmail.com>
2023-12-09 10:57:51 +0530

    add .env to gitignore and add wit ai client to fetch the nlp result query for wolfram understanding

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.21.4
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/shomali11/slacker v1.4.1
+	github.com/tidwall/gjson v1.17.0
+	github.com/wit-ai/wit-go/v2 v2.0.2
 )
 
 require (
@@ -13,4 +15,6 @@ require (
 	github.com/shomali11/commander v0.0.0-20220716022157-b5248c76541a // indirect
 	github.com/shomali11/proper v0.0.0-20180607004733-233a9a872c30 // indirect
 	github.com/slack-go/slack v0.12.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go/v2"
 )
 
-// func printCommandEvents(analyticsChannel <- chan *slacker.CommandEvent) {
-// for event := range analyticsChannel{
-// 	fmt.Println(event.Command)
-// 	fmt.Println(event.Parameters)
-// 	fmt.Println(event.Timestamp)
-// 	fmt.Println(event.Event)}
-// }
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Event)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -26,36 +30,44 @@ func main() {
 	fmt.Println("Hello world")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-	// witClient, _ := wit.NewClient(os.Getenv("WIT_APP_TOKEN"))
+	witClient := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
 	// wolframClient, _ := wit.NewClient(os.Getenv("WOLFRAM_APP_TOKEN"))
 
 	// go printCommandEvents(bot.CommandEvents())
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 
 	bot.Command("query for my bot - <query>", &slacker.CommandDefinition{
 		Description: "ai chat bot",
 		Examples:    []string{"what is the capital of India"},
 		Handler: func(ctx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("query")
-			fmt.Println(query)
 
-			// call wit with the user input to construct query which wolfram understand
-			// res, err := witClient.CommunicateForProcessing(witClient.QueryStruct{
-			// 	Query: query
-			// })
-			// if err != nil {
-			// 	fmt.Println(err.Error())
-			// 	return
-			// }
+			// call wit with the user input to construct query which wolfram can understand
+			res, err := witClient.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			data, err := json.Marshal(res)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			rough := string(data[:])
 
 			// use gjson to deconstruct the response (gjson.ResponseToUnderstandableJson(res, "$wolframquery.wolframquery").0.value) which is  very nested json
+			val := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+			fmt.Println(val)
 
 			// call wolfram with the query result decoded from the wit response
 			response.Reply("Hello world", slacker.WithThreadReply(true))
 		},
 	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	log.Fatal(bot.Listen(ctx))
 }
